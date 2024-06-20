14561df9d198ce27fc92f41d556a1950370420eb
dbish21 <159466540+dbish21@users.noreply.github.com>
2024-06-20 10:41:05 -0400

    Add files via upload

---
+++ object-enhancements/object_enhancements_assignment.js
@@ -0,0 +1,70 @@
+//same keys and values
+function createInstructor(firstName, lastName){
+  return {
+    firstName: firstName,
+    lastName: lastName
+  }
+}
+//ES2015
+function createInstructor(firstName, lastName){
+  return {
+    firstName,
+    lastName
+  }
+}
+//computed property names
+var favoriteNumber = 42;
+
+var instructor = {
+  firstName: "Colt"
+}
+
+instructor[favoriteNumber] = "That is my favorite!"
+//ES2015
+let favoriteNumber = 42;
+
+const instructor = {
+  firstName: "Colt",
+  [favoriteNumber]: "That is my favorite!"
+}
+//object methods
+var instructor = {
+  firstName: "Colt",
+  sayHi: function(){
+    return "Hi!";
+  },
+  sayBye: function(){
+    return this.firstName " + "says bye!";
+  }
+}
+//ES2015
+const instructor = {
+  firstName: "Colt",
+  sayHi(){
+    return "Hi!";
+  },
+  sayBye(){
+    return this.firstName + " says bye!";
+  }
+}
+//createanimal function
+//Write a function which generates an animal object. The function should accepts 3 arguments
+//species: the species of animal (‘cat’, ‘dog’)
+//verb: a string used to name a function (‘bark’, ‘bleet’)
+//noise: a string to be printed when above function is called (‘woof’, ‘baaa’)
+//Use one or more of the object enhancements we’ve covered.
+
+const d = createAnimal("dog", "bark", "Woooof!")
+d.bark()  
+
+const s = createAnimal("sheep", "bleet", "BAAAAaaaa")
+s.bleet() 
+
+function createAnimal(species, verb, noise){
+  return {
+    species,
+    [verb](){
+      return noise;
+    }
+  }
+}
